dce69312204c2f12e2ea0b90293ae6a7b0f7216d
atherfawaz <atherfawaz@users.noreply.github.com>
2020-03-01 19:25:20 +0500

    Valid move condition works.

--- othello.py
+++ othello.py
@@ -25,13 +25,27 @@ def modified_evaluation_function(arguments):
     # do crazy shit here if needed
     dummy = None
 
+def is_adjacent(x, y, turn):
+    opposite_piece = 1 - turn
+    for i in range (x - 1, x + 2):
+        for j in range (y - 1, y + 2):
+            if (i >= 0 and i <= GRID_SIZE and j >= 0 and j <= GRID_SIZE):
+                if (BOARD[i][j] == opposite_piece):
+                    return True
+    return False
 
-def check_valid(x, y):
-    if (x > GRID_SIZE or y > GRID_SIZE):
+
+def check_valid(x, y, turn):
+    if (x >= GRID_SIZE or y >= GRID_SIZE):
         return False
     if (x < 0 or y < 0):
         return False
-    return True  # FIX
+    # Black must place a black disc on the board, 
+    # in such a way that there is at least one straight 
+    # (horizontal, vertical, or diagonal) occupied line 
+    # between the new disc and another black disc, with 
+    # one or more contiguous white pieces between them. 
+    return is_adjacent(x, y, turn) and BOARD[x][y] == -1
 
 
 def continue_game():
@@ -54,17 +68,26 @@ def main():
     BOARD[initial_position + 1][initial_position] = 0  # black bottom-left
 
     display_board()
-    
+    turn = 0
     while (continue_game() is True):
 
         valid_choice = False
-        while(valid_choice is not True):
-            choice_x = int(input("Enter x-coordinate for black: "))
-            choice_y = int(input("Enter y-coordinate for black: "))
-            # check if the player has entered a valid move
-            valid_choice = check_valid(choice_x, choice_y)
-            if (valid_choice is False):
-                print("Invalid choice.")
+        while(valid_choice != True):
+            if (not turn):
+                # human move
+                choice_x = int(input("Enter x-coordinate for black: "))
+                choice_y = int(input("Enter y-coordinate for black: "))
+                # check if the player has entered a valid move
+                valid_choice = check_valid(choice_x - 1, choice_y - 1, turn)
+                if (valid_choice is False):
+                    print("Invalid choice.")
+                # update pieces <add code>
+                turn = not turn
+            else:
+                # AI's move
+                # play with minmax 
+                # <add code>
+                turn = not turn
 
         BOARD[choice_x - 1][choice_y - 1] = 0
         display_board()
